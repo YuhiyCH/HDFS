6db67ac0d6387e1a07d0f98dc57498445ee7b24c
YuhiyCH <35090599+YuhiyCH@users.noreply.github.com>
2024-10-15 14:28:28 +0800

    Create readfile.java

---
+++ readfile.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FSDataInputStream;
+
+public class readfile {
+    public static void main(String[] args) {
+        try {
+            Configuration conf = new Configuration();
+            conf.set("fs.defaultFS","hdfs://192.168.133.131:9000");
+            conf.set("fs.hdfs.impl","org.apache.hadoop.hdfs.DistributedFileSystem");
+            FileSystem fs = FileSystem.get(conf);
+            Path file = new Path("/user/root/1.txt");
+            FSDataInputStream getIt = fs.open(file);
+            BufferedReader d = new BufferedReader(new InputStreamReader(getIt));
+            String content = d.readLine(); //读取文件一行
+            System.out.println(content);
+            d.close(); //关闭文件
+            fs.close(); //关闭hdfs
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
